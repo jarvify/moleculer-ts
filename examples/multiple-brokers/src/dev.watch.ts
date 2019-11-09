7306b2c77e6ee3d0354dd08bc9e006605830bb98
Matus Sabo <lamanabie@gmail.com>
2019-11-09 10:44:34 +0100

    update example multiple-broker dev.watch file

--- examples/multiple-brokers/src/dev.watch.ts
+++ examples/multiple-brokers/src/dev.watch.ts
@@ -1,15 +1,16 @@
-import { generateBrokerWatch, generateBroker } from 'moleculer-ts';
-import { setInterval } from 'timers';
+import { generateBrokerWatch } from 'moleculer-ts';
 
 const brokers = ['first.broker', 'second.broker'];
 
 brokers.forEach(async broker => {
   let brokerRootDir = `${process.cwd()}/src/${broker}`;
 
-  setInterval(async () => {
-    await generateBroker({
-      serviceTypesPattern: `${brokerRootDir}/**/*.service.types.ts`,
-      outputDir: `${brokerRootDir}/types`,
-    });
-  }, 5000);
+  const watcher = await generateBrokerWatch({
+    serviceTypesPattern: `${brokerRootDir}/**/*.service.types.ts`,
+    outputDir: `${brokerRootDir}/types`,
+  });
+
+  watcher.on('change', filename => {
+    console.log(`Generate broker ${broker}, file changed: `, filename);
+  });
 });
